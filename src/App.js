b0ca35f98549f1ea7e7bf0df31b67ef29d165cef
Tauhid <thtauhid.71@gmail.com>
2019-06-29 20:53:18 +0600

    Cleaning old code. Adding routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Route, Router} from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar'
+import ExerciseList from './components/ExerciseList'
+import EditExercise from './components/EditExercise'
+import CreateExercise from './components/CreateExercise'
+import CreateUser from './components/CreateUser'
+
+export default class App extends Component {
+  render() {
+    return(
+        <Router>
+          <Navbar />
+          <br />
+          <Route path="/" exact component={ExerciseList} />
+          <Route path="/edit/:id" component={EditExercise} />
+          <Route path="/create_exercise" component={CreateExercise} />
+          <Route path="/create_user" component={CreateUser} />
+        </Router>
+      )
+  }
 }
-
-export default App;
