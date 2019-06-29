49fc1eb549c4fabf9de17ee9973805978002694c
Tauhid <thtauhid.71@gmail.com>
2019-06-29 21:36:32 +0600

    Creating Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react'
-
+import {Link} from 'react-router-dom'
 export default class Navbar extends Component {
 	render() {
 		return(
-				<div>
-				</div>
+				<nav className="navbar navbar-black bg-dark navbar-expand-lg">
+					<Link to="/" className="navbar-brand" >ExerciseTracker</Link>
+					<div className="collapse navbar-collapse">
+						<ul className="navbar-nav mr-auto">
+							<li className="navbar-item">
+								<Link to="/" className="nav-link" >Exercises</Link>
+							</li>
+							<li className="navbar-item">
+								<Link to="/create_exercise" className="nav-link" >Create Exercise Log</Link>
+							</li>
+							<li className="navbar-item">
+								<Link to="/create_user" className="nav-link" >Create User</Link>
+							</li>
+						</ul>
+					</div>
+				</nav>
 			)
 	}
 }
