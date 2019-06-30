cfd136127235a6df3a6870a6ed95d67f6a15b97c
Tauhid <thtauhid.71@gmail.com>
2019-06-30 08:24:04 +0600

    Linking database to Create Exercise

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import axios from 'axios'
 
 export default class CreateExercise extends Component {
 	constructor(props) {
@@ -23,10 +24,18 @@ export default class CreateExercise extends Component {
 
 
 	componentDidMount() {
-		this.setState({
-			users: ['test user'],
-			username: 'test user'
-		})
+
+		axios.get('http://localhost:5000/user/')
+			.then(response => {
+				if(response.data.length > 0) {
+					this.setState({
+						users: response.data.map(user => user.username),
+						username: response.data[0].username
+					})
+				}
+			})
+
+
 	}
 
 
@@ -67,6 +76,8 @@ export default class CreateExercise extends Component {
 
 		console.log(exercise)
 
+		axios.post('http://localhost:5000/exercise/add', exercise)
+			.then(res => console.log(res.data))
 		// window.location = '/'
 	}
 
