0639bd730398b175d598019d8ebd92d1e363cc2d
Tauhid <thtauhid.71@gmail.com>
2019-06-30 07:14:45 +0600

    updating navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 export default class Navbar extends Component {
 	render() {
 		return(
-				<nav className="navbar navbar-black bg-dark navbar-expand-lg">
+				<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
 					<Link to="/" className="navbar-brand" >ExerciseTracker</Link>
 					<div className="collapse navbar-collapse">
 						<ul className="navbar-nav mr-auto">
