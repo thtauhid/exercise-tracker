a3040d6c9a43fcfee5753a5b253ec4468bf01a10
Tauhid <thtauhid.71@gmail.com>
2019-06-30 09:58:56 +0600

    pushing build directory

---
+++ build/precache-manifest.ce229853d63c95c8344227c840cb1cf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b02b613426ab9cfd3e69768bfb4b79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "706c5a2dae5bd67cf51b",
+    "url": "/static/css/2.89d568a6.chunk.css"
+  },
+  {
+    "revision": "e6c86ea6c6fa767a3818",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "706c5a2dae5bd67cf51b",
+    "url": "/static/js/2.bcb6e094.chunk.js"
+  },
+  {
+    "revision": "e6c86ea6c6fa767a3818",
+    "url": "/static/js/main.a85de70d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
